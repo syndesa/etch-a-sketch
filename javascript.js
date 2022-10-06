d86d34f591701342d0bee7bb064be88d9b7ba471
syndesa <eric.rd@live.com>
2022-10-06 17:22:39 -0400

    Add curson icons for eraser/brush tool & eraser event handler

--- javascript.js
+++ javascript.js
@@ -1,15 +1,30 @@
 const container = document.querySelector('.grid-container');
 const sizeButton = document.querySelector('#gridsize');
 const resetButton = document.querySelector('#reset');
-let mouseIsDown = false; 
+const eraserTool = document.querySelector('#eraser');
+const brushTool = document.querySelector('#color');
 
+let mouseIsDown = false; 
+let selectedTool = null;
 
 document.addEventListener('mousedown', function() {mouseIsDown = true});
 document.addEventListener('mouseup', function() {mouseIsDown = false});
 document.ondragstart = function() {return false};
 
+eraserTool.addEventListener('click', () => {
+    brushTool.style.backgroundColor = 'rgb(223, 222, 222)';
+    selectedTool = 'eraser';
+    eraserTool.style.backgroundColor = 'lightblue';
+    document.body.style.cursor = "url('./eraser.cur'), auto";
+})
 
-// resetButton.addEventListener('click', function() { initGrid(16,16);});
+brushTool.addEventListener('click', ()=>{
+    eraserTool.style.backgroundColor = 'rgb(223, 222, 222)';
+    selectedTool = 'brush';
+    brushTool.style.backgroundColor = 'lightblue';
+    document.body.style.cursor = "url('./Actions-draw-brush-icon.png'), auto";
+})
+
 resetButton.addEventListener('click', clearCanvas);
 
 function clearCanvas(){
@@ -35,18 +50,20 @@ function initGrid(numRows, numCols){
             gridTile.classList.add('grid-item');
             container.appendChild(gridTile);
             gridTile.addEventListener('mousemove', () => {
-                if (mouseIsDown) {gridTile.style.backgroundColor = document.getElementById('color').value;}
+                if (mouseIsDown && selectedTool === 'brush') {gridTile.style.backgroundColor = document.getElementById('color').value;}
+                if (mouseIsDown && selectedTool === 'eraser') {gridTile.style.backgroundColor = '';}
             })
         }        
     }
 }
 
 sizeButton.addEventListener('click', ()=> {
-    
     while(true){
+    
     colSize = prompt('How many columns? (max 100)');
     rowSize = prompt('How many rows? (max 100)');
     if (colSize === null || rowSize === null) break;
+    if (colSize === "" || rowSize === "") break;
     if(colSize>100 || rowSize > 100) {
         alert('Max size exceeded. Try again. ')
         continue
